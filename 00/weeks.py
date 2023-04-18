62665c803a352fb5ac2c3471af771fc0bf2834bc
RafKap <r.kapuscinski95@gmail.com>
2023-04-18 20:19:54 +0200

    Edited file

--- 00/weeks.py
+++ 00/weeks.py
@@ -1,4 +1,17 @@
+#zad 1
+learing_time = 75
+my_time = 2
 
+print("Do nauczenia się nowej umiejętności, potrzebuję ok.", learing_time // my_time, "tygodni")
+
+#zad 2
+min_in_h=60
+h_in_day=24
+day_in_week=7
+
+print("W siedem tyg mamy:", day_in_week * h_in_day * min_in_h * 7, "sekund")
+
+#zad 3
 waga_w_kg = input("Podaj wage w KG: ")
 print("Waga to:",waga_w_kg,"w KG")
 
@@ -11,3 +24,5 @@ wzrost_w_metrach = float(wzrost_w_metrach.replace(",","."))
 
 bmi = waga_w_kg/(wzrost_w_metrach**2)
 print("Twoje BMI to:",round(bmi,2))
+
+#koniec zad3
