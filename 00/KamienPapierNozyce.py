321d204b3af8ccc1fb62d8007cfd40d974f724ec
RafKap <r.kapuscinski95@gmail.com>
2023-04-19 14:07:42 +0200

    New file - RickScissorsPaper game (to be finished), Tests (python self-learning)

---
+++ 00/KamienPapierNozyce.py
@@ -0,0 +1,17 @@
+import random
+
+user_action = input("Enter a choice (rock, paper, scissors): ")
+possible_actions = ["rock", "paper", "scissors"]
+#computer_action = random.choice(possible_actions)
+computer_action="rock"
+
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+if user_action == computer_action:
+    print("Draw")
+elif user_action == "scissors" and computer_action == "rock" or user_action == "paper" and computer_action == "scissors" or user_action == "rock" and computer_action == "paper":
+    print("You lose","\n")
+else:
+    print("You win","\n")
+
+print("End of game")
--- Tests Rk.py
+++ Tests Rk.py
@@ -1,38 +1,42 @@
 print("\n\tHello World!")
-print ()
+print()
 
 # List check
-lst1 = [1,3,5,6,7,8]
-lst2 = [1,2,3,4,5,6,7]
+lst1 = [1, 3, 5, 6, 7, 8]
+lst2 = [1, 2, 3, 4, 5, 7]
 
-lista3 = list(set(lst1)&set(lst2))
+lista3 = list(set(lst1) & set(lst2))
 
-print(lista3)
-print()
-
-
-## String index - powinno rozlozyc RAFAL na kolejne linie w print
+print("\t", lista3, "\n")
 
 
-text_to_index = "Rafal"
+# String index - powinno rozlozyc text na kolejne linie w print
+
+text_to_index = input("Podaj teks do rozbicia: ")
 len(text_to_index)
-#print(len(text_to_index))
+# print(len(text_to_index))
 b = len(text_to_index)
-print(b)
-print()
+print(b, "\n")
 
-
-number = 0
-a = text_to_index[number]
-print(a)
-number = number + 1
-a = text_to_index[number]
-print(a,"\n")
+# number = 0
+# a = text_to_index[number]
+# print(a)
+# number = number + 1
+# a = text_to_index[number]
+# print(a,"\n")
 
 number = 0
-#number = int(number)
+# number = int(number)
 
-while number < 5:
+while number < b:
     a = text_to_index[number]
-    print(a,)
-    number = number +1
+    print(a)
+    number = number + 1
+    if number == b:
+        print()
+
+lcz = [input("Lista: ")]
+for i in lcz:
+    print(i)
+
+choice_list = [1, 2, 3]
