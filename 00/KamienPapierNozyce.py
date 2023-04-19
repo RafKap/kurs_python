fd7ff06cee94c8d93ec27fc8f306b663fd6ed38e
RafKap <r.kapuscinski95@gmail.com>
2023-04-19 20:50:41 +0200

    change lines

--- 00/KamienPapierNozyce.py
+++ 00/KamienPapierNozyce.py
@@ -1,6 +1,6 @@
 import random
 
-user_action = input("Enter a choice (rock, paper, scissors): ")
+user_action = input("Enter a choice (rock, paper, scissors) :")
 possible_actions = ["rock", "paper", "scissors"]
 computer_action = random.choice(possible_actions)
 #computer_action="rock"
@@ -8,15 +8,15 @@ computer_action = random.choice(possible_actions)
 #print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
 
 if user_action not in possible_actions:
-    print("Input not in list")
+    print(user_action, "is not in list", "\n", "End of game")
     exit()
 
 if user_action == computer_action:
-    print("It is draw, because computer choice", computer_action, "is same as Your choice", user_action, "\n")
+    print("It is draw, because computer pick", computer_action, "and it is same as Your pick", user_action, "\n")
 elif user_action == "scissors" and computer_action == "rock" or\
             user_action == "paper" and computer_action == "scissors" or\
             user_action == "rock" and computer_action == "paper":
-        print("You lose, because computer pick", computer_action, "beat You pick", user_action, "\n")
+        print("You lose, because computer pick", computer_action, "and You pick", user_action, "\n")
 else:
     print("You win, because computer pick", computer_action, "and You pick", user_action, "\n")
 
