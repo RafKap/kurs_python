c84304a513d020989c4ef497e3fdf21ed6d41fb6
RafKap <r.kapuscinski95@gmail.com>
2023-05-23 19:21:04 +0200

    Excercise 01

--- 06_Files/FILES_01.py
+++ 06_Files/FILES_01.py
@@ -1,6 +1,17 @@
 import random
 
-with open('FILES_01.txt', encoding="UTF-8") as fopen:
+file_to_open = input("Put file name -> ")
+file_to_open = file_to_open + ".txt"
+with open(file_to_open, encoding="UTF-8") as fopen:
     content_list = fopen.readlines()
     line_today = random.choice(content_list)
-    print(line_today)
+    width = 50
+
+    quote = line_today.split(" - ")
+    quote_today = quote[0]
+    autor = quote[1]
+    print("Quote of the day is: \n")
+    print("*" * width)
+    print(quote_today.strip().center(width))
+    print(autor.strip().center(width))
+    print("*" * width)
